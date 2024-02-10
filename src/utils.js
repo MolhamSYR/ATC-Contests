9ea3c2f1f54bad2ddbe15264aac23f3cd42f3e63
xHurricane <molham.work9@gmail.com>
2024-02-10 17:04:39 +0200

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -2,18 +2,15 @@ const axios = require('axios');
 const { TOKEN, SERVER_URL } = process.env
 const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`
 
-const bot = {
+async function sendMessage(chatid, msgText, threadid) {
 
-sendMessage: async function(chatid, msgText, threadid) {
-
-        axios.post(`${TELEGRAM_API}/sendMessage`, {
+    axios.post(`${TELEGRAM_API}/sendMessage`, {
         chat_id: chatid,
         message_thread_id: threadid,
         text: msgText,
         parse_mode: "HTML"
-        });
-    }
+    });
 
 }
 
-module.exports = {bot};
+module.exports = {sendMessage};
