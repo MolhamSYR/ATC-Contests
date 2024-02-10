9d7dccc5ab1c9c18da190b17ba3176d3d4e5d473
xHurricane <molham.work9@gmail.com>
2024-02-10 17:00:04 +0200

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -4,16 +4,15 @@ const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`
 
 const bot = {
 
-sendMessage(chatid, msgText, threadid) {
+sendMessage: async function(chatid, msgText, threadid) {
 
         axios.post(`${TELEGRAM_API}/sendMessage`, {
         chat_id: chatid,
         message_thread_id: threadid,
         text: msgText,
         parse_mode: "HTML"
-    });
-
-}
+        });
+    }
 
 }
 
